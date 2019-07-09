0558a35ae1c7c571a291deb39c546f4839f0715d
jerryhanhuan <dengfeng2012@163.com>
2019-07-09 00:22:46 +0000

    add aticle_controller create action model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,10 @@ class ArticlesController < ApplicationController
 
 
 	def create
-		render plain: params[:article].inspect
+		#render plain: params[:article].inspect
+		@article = Article.new(params[:article])
+		@article.save
+		redirect_to @article
 	end
 
 end
