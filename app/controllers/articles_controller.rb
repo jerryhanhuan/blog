c08c83abf025d247abcb86cab194824619b723e4
jerryhanhuan <dengfeng2012@163.com>
2019-07-10 00:42:12 +0000

    add index and show action and erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,33 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
+
 	def new
 	end
 
 
 	def create
 		#render plain: params[:article].inspect
-		@article = Article.new(params[:article])
+		#@article = Article.new(params[:article])
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to @article
+		#redirect_to :action => 'show'
+		#render plain: @article.inspect
 	end
+
+
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+private
+	def article_params
+		params.require(:article).permit(:title,:text)
+	end
+
 
 end
